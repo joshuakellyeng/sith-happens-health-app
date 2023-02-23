cc3335f8ce9bdc5ccfa23e2e7b509f3889f3928a
Joshua Kelly <joshuakellyeng@gmail.com>
2023-02-23 16:24:57 -0500

    Health app first commit

---
+++ app/src/main/java/com/demo/jetpackcomposecrashcourse/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.demo.jetpackcomposecrashcourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.demo.jetpackcomposecrashcourse.ui.theme.JetpackcomposecrashcourseTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackcomposecrashcourseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+        .fillMaxSize()
+
+    ) {
+
+        Text(
+            text = "Welcome back $name!",
+            color= Color.Blue,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(15.dp,52.dp)
+        )
+    }
+    
+    Row(horizontalArrangement = Alignment.Start) {
+        
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackcomposecrashcourseTheme {
+        Greeting("Joshua")
+    }
+}
